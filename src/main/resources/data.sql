fa239a581fb28097aa6f802cfb983ae25041b67c
Rajesh G <rajesh@unigps.in>
2022-01-13 02:12:05 +0530

    refine

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO ACCOUNT(account_Id,customer_Id,current_Balance,account_Type,owner_Name)
+INSERT INTO account(account_Id,customer_Id,current_Balance,account_Type,owner_Name)
 VALUES (10000,'eric',100000.0, 'Savings', 'Eric D'),
 	(20000,'john',100000.0, 'Savings', 'John Rhodes'),
 	(20001,'john',20000.0, 'Current', 'John Rhodes'),
